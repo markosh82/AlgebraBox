fea9d14134cb4149db4600c2a124605e8921bcb5
online1 <online1@ucione.local>
2017-05-05 18:41:10 +0200

    Fix test bug

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    //use CreatesApplication;
+	
+	public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
 }
